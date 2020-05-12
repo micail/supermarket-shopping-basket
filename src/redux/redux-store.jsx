894875a4a46e7b4fa12315f341a5bb0d0e4cfdfa
micail <michaelpaczkowski@gmail.com>
2020-05-12 19:32:10 +0100

    Redux store code clean up

--- src/redux/redux-store.jsx
+++ src/redux/redux-store.jsx
@@ -11,11 +11,17 @@ import priceListReducer from './reducers/priceListReducer';
 import receiptReducer from './reducers/receiptReducer';
 import totalsReducer from './reducers/totalsReducer';
 
-import { addItem, calculatePriceOnWeight, calculateSubTotal, calculateSavings } from './middleware';
+import priceList from '../data/priceList';
+
+import {
+  addItem,
+  calculatePriceOnWeight,
+  calculateSubTotal,
+  loadPriceList,
+} from './middleware';
 
 const pipeLine = (store) => (next) => (action) => {
   const prevState = store.getState();
-  const prevReceiptReducerState = [...prevState.receiptReducer];
 
   if ((action.type === 'ADD_ITEM' && Array.isArray(action.index)) && action.index[0].length === 2) {
     const newData = calculatePriceOnWeight(prevState, action.index);
@@ -25,13 +31,10 @@ const pipeLine = (store) => (next) => (action) => {
   next(action);
 
   const nextState = store.getState();
-  const nextReceiptReducerState = [...nextState.receiptReducer];
-
   /** Calculate sub-total after every item has been added to the store */
   if (action.type === 'ADD_ITEM') {
     calculateSubTotal(store, nextState);
   }
-  console.log(nextState)
 };
 
 /**
@@ -43,15 +46,15 @@ const pipeLine = (store) => (next) => (action) => {
 
 export const makeStore = () => {
   const reducers = {
-    priceListReducer,
-    receiptReducer,
-    totalsReducer,
+    priceList: priceListReducer,
+    receipt: receiptReducer,
+    totals: totalsReducer,
   };
 
   const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
   const applyMid = composeEnhancers(applyMiddleware(pipeLine));
   const store = createStore(combineReducers(reducers), applyMid);
-
+  loadPriceList(store, priceList);
   return store;
 };
 
